286c652d5b89ae9102f42128a28ffeedd2c803f7
Steven Kalt <kalt.steven@gmail.com>
2024-03-09 10:59:21 -0500

    fix: make go benchmark more fair

--- internal/reference_oracle/bench_test.go
+++ internal/reference_oracle/bench_test.go
@@ -14,18 +14,34 @@ import (
 
 //go:embed inputs.txt
 var rawInputs string
-var inputs = strings.Split(rawInputs, "\n")
 
 func BenchmarkOracleEntireTestSuite(b *testing.B) {
-	filtered := make([]string, 0, len(inputs))
+	var inputs = strings.Split(rawInputs, "\n")
+	var filtered = make([]string, 0, len(inputs))
 	for _, ref := range inputs {
 		if ref != "" {
 			filtered = append(filtered, ref)
 		}
 	}
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		for _, ref := range filtered {
 			reference.Parse(ref)
+		}
+	}
+}
+
+func BenchmarkJustIteration(b *testing.B) {
+	var inputs = strings.Split(rawInputs, "\n")
+	var filtered = make([]string, 0, len(inputs))
+	for _, ref := range inputs {
+		if ref != "" {
+			filtered = append(filtered, ref)
+		}
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for _, _ = range filtered {
 		}
 	}
 }
